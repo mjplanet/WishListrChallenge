bc080a4866429cc6cf56e77a2c57ebd3e8666b24
Mobin Jahantark <jahantark@me.com>
2022-09-17 23:00:00 +0430

    Add - CollectionView layout created.

--- WishListrChalenge/MovieList/MovieListView.swift
+++ WishListrChalenge/MovieList/MovieListView.swift
@@ -16,23 +16,75 @@ class MovieListView: UIViewController {
     
     var presenter: MovieListPresenterInterface!
     
-    
+    enum Section {
+        case main
+    }
+
     // MARK: - Properties
-    
-    
+    private var collectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Section, AnyHashable>!
+
     
     // MARK: - Lifecycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         presenter.viewDidLoad()
         
+        configureNavItem()
+        configureHierarchy()
     }
     
     
     // MARK: - Setups
+    private func configureNavItem() {
+        navigationItem.title = "Movie list"
+        navigationItem.largeTitleDisplayMode = .always
+    }
     
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemGroupedBackground
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let sectionProvider = { [weak self] (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            return self.createCollectionSection()
+        }
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.interSectionSpacing = 16
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider, configuration: configuration)
+    }
+    
+    private func createCollectionSection() -> NSCollectionLayoutSection {
+        
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalWidth(1))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                               heightDimension: .fractionalWidth(1))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.interGroupSpacing = 8
+        
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+        
+        return section
+    }
     
     
     // MARK: - Theme
@@ -54,4 +106,10 @@ extension MovieListView: MovieListViewInterface {
     
 }
 
-
+extension MovieListView: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+}
